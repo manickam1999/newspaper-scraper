7b37fb7b3311e6a6dac998dfc4aa36d7cfd4393f
Manickam Murugappan <manickam.murugappan@vitrox.com>
2025-04-05 12:10:49 +0800

    feat: add --disable-dev-shm-usage option to Chrome driver setup for improved stability

--- src/scraper.py
+++ src/scraper.py
@@ -14,6 +14,7 @@ def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--start-maximized")
